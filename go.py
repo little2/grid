bf2ee2e91516b5ebd358245b6179e92b0f630727
little2 <agile@little2.net>
2025-09-08 07:48:20 +0800

    update

--- go.py
+++ go.py
@@ -7,8 +7,8 @@ hg = HeroGridVideo(font_path="fonts/Roboto_Condensed-Regular.ttf",
 
 
 meta = hg.generate(
-    video_path="video/Untitled2.mp4",
-    preview_basename="previews/110763",
+    video_path="video/20241215_002503.MP4",
+    preview_basename="previews/51027",
     # manual_times=["07:13"],
     # sample_count=180,
     # num_aux=12,
